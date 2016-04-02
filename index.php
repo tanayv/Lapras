a63310bae521cb5d85dc529e66145fa03f009a00
Tanay Vardhan <tvardha2@illinois.edu>
2016-04-02 04:59:56 +0000

    First PHP index

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>Lapras</title>
+	<link rel='stylesheet' type='text/css' href='login.css'>
+	<meta charset='UTF-8'>
+</head>
+<body>
+
+	<div id='container'>
+
+		<h1>Lapras</h1>
+
+		<div id='sign_in'>
+
+			<input id='mail' type='email' name='email' placeholder='Email ID'>
+			<input id='psswd' type='password' name='password' placeholder='Password'>
+			<input id='si_bttn' type='button' value='SIGN IN'>
+
+		</div>
+
+	</div>
+
+</body>
+</html>
